345a1c8112da4142eafcfd4ddcbaf8702324c400
ArielFeingold <feingold.ariel@gmail.com>
2019-01-11 11:54:40 -0500

    user model set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :recipes
+
+  validates :email, :username, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 8 }
 end
